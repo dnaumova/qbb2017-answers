df8d688776f94056597a3b74ea25163a4aa5a709
Daria Naumova <naumovadariaa@gmail.com>
2017-09-14 21:37:00 -0400

    making alignment

---
+++ week-1/newalign.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import sys
+import itertools
+import fasta
+
+nukes = open(sys.argv[1])
+protes = open(sys.argv[2])
+
+nukeprot = open("nukeprot.fa","w")
+
+for (nident, nseq), (pident, pseq) in itertools.izip(fasta.FASTAReader(nukes), fasta.FASTAReader(protes)):
+    nukeprot.write(nident + "\n")
+    for aa in pseq:
+        if aa == "-":
+            nukeprot.write(nseq[:3])
+        else:
+            nukeprot.write(nseq[:3])
+            nseq = nseq[3:]
